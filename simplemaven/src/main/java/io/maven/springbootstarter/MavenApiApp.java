f978d9e9719c242f9b025cb2eb0c9da461e45d77
Srishti <srishtipoudel2385@gmail.com>
2023-01-21 07:40:03 +0545

    added main class

---
+++ simplemaven/src/main/java/io/maven/springbootstarter/MavenApiApp.java
@@ -0,0 +1,14 @@
+package io.maven.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApiApp.class, args);
+
+	}
+
+}
